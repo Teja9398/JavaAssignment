d8ee03b55723ff20fd611e0dc3e7dc59c701587c
Teja9398 <dharmateja9398@gmail.com>
2023-07-27 09:46:03 +0530

    minor changes

--- JavaAssignmentPrograms/Part4/Program4_3.java
+++ JavaAssignmentPrograms/Part4/Program4_3.java
@@ -37,7 +37,7 @@ class Thread1 extends Thread{
 
 public class Program4_3 {
     
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args){
         Thread1 t1=new Thread1();
         Thread2 t2=new Thread2();
         Thread3 t3=new Thread3();
