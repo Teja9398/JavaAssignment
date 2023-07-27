15ab2e4b653a7e77c755ad32b84057808c45dd3c
Teja9398 <dharmateja9398@gmail.com>
2023-07-27 12:18:27 +0530

    New program Added

---
+++ JavaAssignmentPrograms/Part4/Program4_7b.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+public class Program4_7b {
+    public static void main(String[] args) {
+        JFrame frame =new JFrame("Canvas");
+        frame.setBounds(400,200,720,480);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+
+        frame.setVisible(true);
+    }
+}
