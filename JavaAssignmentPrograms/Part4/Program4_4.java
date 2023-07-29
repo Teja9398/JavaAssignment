1d5251e3c4aa46985f0b5e6f26d4e3bba7b6de58
Teja9398 <dharmateja9398@gmail.com>
2023-07-29 23:06:29 +0530

    minor changes

--- JavaAssignmentPrograms/Part4/Program4_4.java
+++ JavaAssignmentPrograms/Part4/Program4_4.java
@@ -1,45 +1,78 @@
+import java.util.Scanner;
+
 class Buffer{
-    static int i=-1;
-    static final int size=3;
+    static int size;
+    static int i=0;
+
+    Buffer(int size){
+        this.size=size;
+    }
+    
+    public synchronized void produce(){
+        if(i==size){
+            System.out.println("Buffer if full");
+            try{
+                wait();
+            }catch(InterruptedException i){}
+        }
+        i++;
+        System.out.println("produced: "+i);
+        notify();
+    }
+    public synchronized void consume(){
+        if(i==0){
+            System.out.println("Buffer is empty.");
+            try {
+                wait();
+            } catch (InterruptedException e) {}
+        }
+        i--;
+        System.out.println("consumed: "+i);
+        notify();
+
+    }
 }
 
 class Producer extends Thread{
+    Buffer b;
+    Producer(Buffer b){
+        this.b=b;
+    }
     
     public void run(){
-        synchronized(this){
-            Buffer.i++;
-            while(Buffer.i==Buffer.size){
-                    try{
-                        wait();
-                    }catch(InterruptedException ie){}
-                }
-                    System.out.println("producer produced "+(Buffer.i+1));
-                    notify();
-            
+       for(int i=0;i<Buffer.size;i++){
+            b.produce();
+            try{
+                Thread.sleep(100);
+            }catch(InterruptedException e){}
         }
     }
 }
 
 class Consumer extends Thread{
+    Buffer b;
+    Consumer(Buffer b){
+        this.b=b;
+    }
+
     public void run(){
-        synchronized(this){
-            Buffer.i--;
-            while(Buffer.i==-1){
-                    try{
-                        wait();
-                    }catch(InterruptedException ie){}
-            }
-            System.out.println("consumer consumed item "+(Buffer.i+1));
-            notify();
-            
+        for(int i=0;i<Buffer.size;i++){
+            b.consume();
+            try{
+                Thread.sleep(2000);
+            }catch(InterruptedException e){}
         }
     }
 }
 
 public class Program4_4 {
     public static void main(String[] args) {
-        Producer p=new Producer();
-        Consumer c=new Consumer();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size of the buffer :");
+        int s= sc.nextInt();
+        Buffer b=new Buffer(s);
+        Producer p=new Producer(b);
+        Consumer c=new Consumer(b);
         p.start();
         c.start();
     }
